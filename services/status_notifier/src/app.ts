03311fbd66eb2beb6232bf908c2634c1c0b714c0
Nerbyk <danil.maximov@flatstack.com>
2022-04-26 18:14:05 +0200

    remove notused aws-sdk for status_notifier service

--- services/status_notifier/src/app.ts
+++ services/status_notifier/src/app.ts
@@ -1,8 +1,5 @@
-import AWS from 'aws-sdk';
 import TelegramNotifier from './notifiers/TelegramNotifier';
 import { serializeDynamodbRecord } from './helpers/dynamodb';
-
-AWS.config.update({ region: 'eu-central-1' });
 
 export const lambdaHandler = async (event) => {
   const statusData = serializeDynamodbRecord(event.Records[0]);
